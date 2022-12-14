12dcdf51bbe6ac2901f7d107e4555bacef655e41
cheahang-99 <100119039+cheahang-99@users.noreply.github.com>
2022-12-14 10:04:22 +0800

    feat(main): banner content and cta

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,26 @@ function App() {
     <section>
       <div className="container">
         <Navigation />
+
+        <div className="banner__container">
+          <div className="banner__content">
+            <h1>Make remote work</h1>
+            <p>Get your team in sync, no matter your location. Streamline processes,
+              create team rituals, and watch productivity soar.</p>
+
+            <button href="#">Learn more</button>
+
+            <div className="banner__clients">
+              <img src="images/client-databiz.svg" alt="client-databiz" height="16" />
+              <img src="images/client-audiophile.svg" alt="client-audiophile" height="24" />
+              <img src="images/client-meet.svg" alt="client-meet" height="16" />
+              <img src="images/client-maker.svg" alt="client-maker" heigh="16" />
+            </div>
+          </div>
+
+          <img className="banner__image" src="images/image-hero-desktop.png" alt="" />
+          <img className="banner__image_mobile" src="images/image-hero-mobile.png" alt="" />
+        </div>
       </div>
     </section >
   )
